5169a228a9cca3d7a3d5052b6e10f5137382ff0f
Chris Payne <chrispayneseo@gmail.com>
2026-09-25 14:12:15 +0100

    Register Phase 4K acceptance route

--- dell-node/core/app/__init__.py
+++ dell-node/core/app/__init__.py
@@ -9,12 +9,14 @@ from . import proactive_schedule as proactive_schedule
 from . import proactive_preferences as proactive_preferences
 from . import proactive_delivery as proactive_delivery
 from . import proactive_feedback as proactive_feedback
+from . import proactive_acceptance as proactive_acceptance
 
 proactive_preferences.register_routes()
 proactive_brief.register_routes()
 proactive_schedule.register_routes()
 proactive_delivery.register_routes()
 proactive_feedback.register_routes()
+proactive_acceptance.register_routes()
 proactive_schedule.install_background_loop()
 proactive_preferences.install_runtime_hooks()
 proactive_feedback.install_hooks()
